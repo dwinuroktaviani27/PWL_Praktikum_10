366c8d7f7bd335b54d6c2322b7bbb4392d8f93fe
dwinuroktaviani27 <1941720239@student.polinema.ac.id>
2021-04-19 22:49:06 +0700

    Mengubah fungsi up()

--- database/migrations/2021_04_19_154159_create_articles_table.php
+++ database/migrations/2021_04_19_154159_create_articles_table.php
@@ -15,7 +15,7 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->sring('title');
+            $table->string('title');
             $table->text('content');
             $table->string('featured_image');
             $table->timestamps();
